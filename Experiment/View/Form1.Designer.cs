6c904adfe312f40ebfa3dde471804b3c883db188
MarkKochRuge <165993382+MarkKochRuge@users.noreply.github.com>
2024-05-14 08:59:43 +0200

    ændring

--- Experiment/View/Form1.Designer.cs
+++ Experiment/View/Form1.Designer.cs
@@ -56,8 +56,9 @@
             btnPanel.Controls.Add(panel2);
             btnPanel.Dock = DockStyle.Left;
             btnPanel.Location = new Point(0, 0);
+            btnPanel.Margin = new Padding(3, 4, 3, 4);
             btnPanel.Name = "btnPanel";
-            btnPanel.Size = new Size(200, 656);
+            btnPanel.Size = new Size(229, 875);
             btnPanel.TabIndex = 0;
             // 
             // button4
@@ -70,10 +71,11 @@
             button4.ForeColor = Color.White;
             button4.Image = (Image)resources.GetObject("button4.Image");
             button4.ImageAlign = ContentAlignment.MiddleLeft;
-            button4.Location = new Point(0, 307);
+            button4.Location = new Point(0, 409);
+            button4.Margin = new Padding(3, 4, 3, 4);
             button4.Name = "button4";
-            button4.Padding = new Padding(10, 0, 0, 0);
-            button4.Size = new Size(200, 75);
+            button4.Padding = new Padding(11, 0, 0, 0);
+            button4.Size = new Size(229, 100);
             button4.TabIndex = 8;
             button4.Text = "   Mægler";
             button4.TextAlign = ContentAlignment.MiddleLeft;
@@ -91,10 +93,11 @@
             button3.ForeColor = Color.White;
             button3.Image = (Image)resources.GetObject("button3.Image");
             button3.ImageAlign = ContentAlignment.MiddleLeft;
-            button3.Location = new Point(0, 232);
+            button3.Location = new Point(0, 309);
+            button3.Margin = new Padding(3, 4, 3, 4);
             button3.Name = "button3";
-            button3.Padding = new Padding(10, 0, 0, 0);
-            button3.Size = new Size(200, 75);
+            button3.Padding = new Padding(11, 0, 0, 0);
+            button3.Size = new Size(229, 100);
             button3.TabIndex = 7;
             button3.Text = "   Køber";
             button3.TextAlign = ContentAlignment.MiddleLeft;
@@ -112,10 +115,11 @@
             button2.ForeColor = Color.White;
             button2.Image = (Image)resources.GetObject("button2.Image");
             button2.ImageAlign = ContentAlignment.MiddleLeft;
-            button2.Location = new Point(0, 157);
+            button2.Location = new Point(0, 209);
+            button2.Margin = new Padding(3, 4, 3, 4);
             button2.Name = "button2";
-            button2.Padding = new Padding(10, 0, 0, 0);
-            button2.Size = new Size(200, 75);
+            button2.Padding = new Padding(11, 0, 0, 0);
+            button2.Size = new Size(229, 100);
             button2.TabIndex = 6;
             button2.Text = "   Sælger";
             button2.TextAlign = ContentAlignment.MiddleLeft;
@@ -130,9 +134,10 @@
             button5.FlatStyle = FlatStyle.Flat;
             button5.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
             button5.ForeColor = Color.White;
-            button5.Location = new Point(0, 611);
+            button5.Location = new Point(0, 815);
+            button5.Margin = new Padding(3, 4, 3, 4);
             button5.Name = "button5";
-            button5.Size = new Size(200, 45);
+            button5.Size = new Size(229, 60);
             button5.TabIndex = 5;
             button5.Text = "Admin Login";
             button5.UseVisualStyleBackColor = true;
@@ -147,10 +152,11 @@
             button1.ForeColor = Color.White;
             button1.Image = (Image)resources.GetObject("button1.Image");
             button1.ImageAlign = ContentAlignment.MiddleLeft;
-            button1.Location = new Point(0, 82);
+            button1.Location = new Point(0, 109);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Padding = new Padding(10, 0, 0, 0);
-            button1.Size = new Size(200, 75);
+            button1.Padding = new Padding(11, 0, 0, 0);
+            button1.Size = new Size(229, 100);
             button1.TabIndex = 1;
             button1.Text = "   Bolig";
             button1.TextAlign = ContentAlignment.MiddleLeft;
@@ -165,19 +171,20 @@
             panel2.Controls.Add(logoPnl);
             panel2.Dock = DockStyle.Top;
             panel2.Location = new Point(0, 0);
+            panel2.Margin = new Padding(3, 4, 3, 4);
             panel2.Name = "panel2";
-            panel2.Size = new Size(200, 82);
+            panel2.Size = new Size(229, 109);
             panel2.TabIndex = 0;
             // 
             // logoPnl
             // 
             logoPnl.BackColor = Color.FromArgb(105, 54, 158);
             logoPnl.Dock = DockStyle.Top;
-            logoPnl.Font = new Font("MV Boli", 42F, FontStyle.Bold);
+            logoPnl.Font = new Font("Myanmar Text", 42F, FontStyle.Bold, GraphicsUnit.Point, 0);
             logoPnl.ForeColor = Color.Transparent;
             logoPnl.Location = new Point(0, 0);
             logoPnl.Name = "logoPnl";
-            logoPnl.Size = new Size(200, 82);
+            logoPnl.Size = new Size(229, 109);
             logoPnl.TabIndex = 0;
             logoPnl.Text = "EDC";
             logoPnl.TextAlign = ContentAlignment.MiddleCenter;
@@ -189,9 +196,10 @@
             panel1.Controls.Add(labelonp);
             panel1.Dock = DockStyle.Top;
             panel1.ForeColor = Color.White;
-            panel1.Location = new Point(200, 0);
+            panel1.Location = new Point(229, 0);
+            panel1.Margin = new Padding(3, 4, 3, 4);
             panel1.Name = "panel1";
-            panel1.Size = new Size(888, 82);
+            panel1.Size = new Size(1014, 109);
             panel1.TabIndex = 1;
             // 
             // labelonp
@@ -199,9 +207,9 @@
             labelonp.AutoSize = true;
             labelonp.Font = new Font("Myanmar Text", 23.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             labelonp.ForeColor = Color.Black;
-            labelonp.Location = new Point(22, 19);
+            labelonp.Location = new Point(8, 16);
             labelonp.Name = "labelonp";
-            labelonp.Size = new Size(109, 55);
+            labelonp.Size = new Size(135, 70);
             labelonp.TabIndex = 0;
             labelonp.Text = "HJEM";
             labelonp.TextAlign = ContentAlignment.MiddleCenter;
@@ -211,20 +219,22 @@
             Screenpanel.AutoSize = true;
             Screenpanel.BackColor = Color.White;
             Screenpanel.Dock = DockStyle.Fill;
-            Screenpanel.Location = new Point(200, 82);
+            Screenpanel.Location = new Point(229, 109);
+            Screenpanel.Margin = new Padding(3, 4, 3, 4);
             Screenpanel.Name = "Screenpanel";
-            Screenpanel.Size = new Size(888, 574);
+            Screenpanel.Size = new Size(1014, 766);
             Screenpanel.TabIndex = 2;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(240, 236, 229);
-            ClientSize = new Size(1088, 656);
+            ClientSize = new Size(1243, 875);
             Controls.Add(Screenpanel);
             Controls.Add(panel1);
             Controls.Add(btnPanel);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Form1";
             btnPanel.ResumeLayout(false);
